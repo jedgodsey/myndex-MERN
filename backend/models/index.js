11805d357d8d6b864706588d844b6a61c9308e5f
jedgodsey <jed.godsey@gmail.com>
2020-11-28 11:29:28 -0800

    started putting backend together

---
+++ backend/models/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/myndex", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('MongoDB connected...');
+})
+
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+})
+
+module.exports = {
+  Myndex: require('./myndex'),
+  Profile: require('./profile')
+}
