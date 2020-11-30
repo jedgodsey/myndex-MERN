67c5aed8e44db6e81c220cf76a213e6c5d03f495
jedgodsey <jed.godsey@gmail.com>
2020-11-30 09:39:35 -0800

    single day performance working

--- frontend/src/components/IndexCard.js
+++ frontend/src/components/IndexCard.js
@@ -4,6 +4,9 @@ import MyndexModel from '../models/myndex';
 import Tradier from '../models/tradier';
 
 class IndexCard extends React.Component {
+  state = {
+    performance: 0
+  }
 
   componentDidMount() {
     this.pullQuotes(this.props.index.holdings)
@@ -14,9 +17,18 @@ class IndexCard extends React.Component {
   }
 
   pullQuotes = (tickers) => {
-    console.log(this.props.index.holdings)
     Tradier.fill(tickers.join()).then(res => {
-      console.log((res.data.quotes.quote.reduce((a, b) => a + b.change_percentage, 0)) / res.data.quotes.quote.length) // change this reduce to a loop or something
+      let average = 0;
+      if (res.data.quotes.quote.change_percentage) {
+        average = res.data.quotes.quote.change_percentage
+      } else {
+        let total = 0
+        for (let i = 0; i < res.data.quotes.quote.length; i++) {
+          total += res.data.quotes.quote[i].change_percentage
+        }
+        average = total / res.data.quotes.quote.length
+      }
+      this.setState({performance: average})
     })
   }
 
@@ -25,6 +37,7 @@ class IndexCard extends React.Component {
       <div className="index-card">
         ID#: {this.props.index._id}
         <p>{this.props.index.holdings}</p>
+        <p>Today's performance: {this.state.performance}</p>
         <button onClick={() => this.onDelete(this.props.index._id)}>delete the above index</button>
         <Link to={`/myndeces/${this.props.index._id}/edit`}>
           <p>edit the above index</p>
