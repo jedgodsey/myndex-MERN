5995b8239c31fc42b43842d65b5e350b2ab725e7
jedgodsey <jed.godsey@gmail.com>
2020-12-01 22:22:57 -0800

    updated mongo models

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,16 +2,12 @@ const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
-  indexIDs: Array,
   email: String,
-  familyName: String,
-  givenName: String,
   googleId: String,
   imageUrl: String,
   name: String,
   company: String,
   logo: String,
-  username: String,
   password: String
 }, {timestaps: true})
 
--- backend/models/myndex.js
+++ backend/models/myndex.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 const myndexSchema = new mongoose.Schema({
   indexName: String,
-  holdings: Array
+  holdings: Array,
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Creator'
+  },
+  weighting: {
+    type: String,
+    default: 'Even'
+  },
+  startDate: Date
 }, {timestaps: true})
 
 const Index = mongoose.model('Index', myndexSchema);
