9f5f66be8f11a9d929f685c566a03ec95f0c836d
jedgodsey <jed.godsey@gmail.com>
2020-12-10 12:11:04 -0800

    committing backend to its own repository

--- frontend/src/components/ListItem.js
+++
@@ -1,7 +0,0 @@
-const ListItem = (props) => {
-  return(
-    <li onClick={() => {props.pickTicker(props.stock.symbol)}}>{`${props.stock.symbol}: ${props.stock.description}`}</li>
-  )
-}
-
-export default ListItem;
