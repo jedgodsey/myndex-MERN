f62e3a94bbf85624abb5802354ed4230c8010377
jedgodsey <jed.godsey@gmail.com>
2020-12-11 15:07:55 -0800

    committing to push

--- index.js
+++ index.js
@@ -101,5 +101,12 @@ app.get('/logout', (req, res) => {
   res.redirect('/')
 })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // connection
 app.listen(port, () => console.log(`Server is running on port ${port}`));
